f7a596a923aae40a9c0b7f975d20729a588f3174
Shiwon Park <psw041115@gmail.com>
2024-03-27 01:51:08 +0900

    test: idp service

--- test/idp/idp.service.spec.ts
+++ test/idp/idp.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IdpService } from '../../src/idp/idp.service';
+import { testConfigModule } from 'test/config/testConfig.module';
+import { HttpModule } from '@nestjs/axios';
 
 describe('IdpService', () => {
   let service: IdpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [testConfigModule, HttpModule],
       providers: [IdpService],
     }).compile();
 
