62120d426ef2ae50c3062d551c145900ef21c8d7
siwonpada <psw041115@gmail.com>
2024-04-23 21:05:41 +0900

    processing

--- test/idp/idp.service.spec.ts
+++ test/idp/idp.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IdpService } from '../../src/idp/idp.service';
+import { testConfigModule } from 'test/config/testConfig.module';
+import { HttpModule } from '@nestjs/axios';
 
 describe('IdpService', () => {
   let service: IdpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [testConfigModule, HttpModule],
       providers: [IdpService],
     }).compile();
 
@@ -14,5 +17,9 @@ describe('IdpService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getUserInfo', () => {
+    it('should return user info', async () => {});
   });
 });
